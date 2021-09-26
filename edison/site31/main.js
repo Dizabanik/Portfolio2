9dae6dd92f8e7454ccbd4eb14931033409a4fcad
Dizabanik <dizabanik@gmail.com>
2021-09-26 16:13:40 +0300

    New Sites

---
+++ edison/site31/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", ()=>{
+    const preloader = document.querySelector(".preloader");
+    preloader.classList.add("preload-finish");
+
+    
+
+    // setInterval(()=>{
+    //     popup.style.display = "block";
+    // }, 4000);
+});
